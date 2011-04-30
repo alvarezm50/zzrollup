da7a2696fdc3fa0d7ac4b480104f5a1014fdde9f
gregseitz <gseitz@gmail.com>
2011-04-29 18:51:38 -0700

    hot restart if already running

--- run_unicorn.rb
+++ run_unicorn.rb
@@ -1,10 +1,24 @@
 #!/usr/bin/env ruby
 
+# we either restart unicorn if it is running with USR2 or if not running
+# we start it
+
 def do_cmd(cmd)
   puts cmd
   Kernel.system(cmd)
 end
 
 app_dir = Dir.pwd
+pid_file = "/var/run/zangzing/unicorn_rollup.pid"
 
-do_cmd "unicorn -E production -c #{app_dir}/config/unicorn.rb -D #{app_dir}/config.ru"
+do_cmd "ps -fp `cat #{pid_file}`"
+running = $?.exitstatus == 0
+
+if running
+  # restart gracefully
+  do_cmd "kill -s USR2 `cat #{pid_file}`"
+else
+  # start from scratch
+  do_cmd "unicorn -E production -c #{app_dir}/config/unicorn.rb -D #{app_dir}/config.ru"
+end
+
