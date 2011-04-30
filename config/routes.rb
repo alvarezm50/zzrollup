2abd2a67ee74e7a45abda9b3b79d0163fe321d2f
gregseitz <gseitz@gmail.com>
2011-04-30 09:22:49 -0700

    gk

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rollup::Application.routes.draw do
-  get    '/daily_report'                  => 'results#daily_report_file',      :as => :daily_report_file
+  get    '/daily_report'                  => 'results#daily_report_file',       :as => :daily_report_file
   get    '/daily_report_table'            => 'results#daily_report_table',      :as => :daily_report_table
+
+  # geckoboard
+  get    '/gecko_before_after'            => 'gecko#before_after',              :as => :gecko_before_after
 end
