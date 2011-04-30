81718a4954e012ec9cf3cdbfad78cc08d3b92b48
gregseitz <gseitz@gmail.com>
2011-04-29 23:07:13 -0700

    using send data instead of send file

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -8,14 +8,14 @@ class ResultsController < ApplicationController
     zip_it = false
     t = RollupTasks.create_csv(span, zip_it, base_file_name)
 
-    RollupTasks.dump_file(t.path)
-
     # send it
     if zip_it
       send_file t.path, :type => 'application/zip', :filename => "#{base_file_name}.zip"
     else
       f = File.open(t.path, "rb")
       buff = f.read()
+      f.close
+      # trying to use send_file fails to operate, could have something to do with using temp file
       send_data buff, :type => 'text/csv', :filename => "#{base_file_name}.csv"
       #send_file t.path, :type => 'text/csv', :filename => "#{base_file_name}.csv"
     end
