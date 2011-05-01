9e03b807f50e510e64982c86d2b1e4e2f85089b9
gregseitz <gseitz@gmail.com>
2011-05-01 08:37:37 -0700

    Set name of query for agen.first.use

--- lib/evt_sweep.rb
+++ lib/evt_sweep.rb
@@ -5,7 +5,7 @@ class EvtSweep < BaseSweep
 
   def self.full(span)
     minimal(span)
-    full_query(db, "Page.visit.user.all", span,
+    full_query(db, "Agent.first.use.all", span,
                "SELECT count(*) FROM evts WHERE event = 'agent.first.use'")
     grouped_query(db, "Page.visit.user", span,
                "SELECT user_type,count(id) FROM evts WHERE event = 'page.visit' AND source = 1 GROUP BY user_type ORDER BY user_type")
