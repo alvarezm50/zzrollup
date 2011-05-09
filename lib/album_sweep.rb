1ea743939697889a64c4cb001ae8d3b659a94b53
gregseitz <gseitz@gmail.com>
2011-05-09 10:07:48 -0700

    Add counts by album type

--- lib/album_sweep.rb
+++ lib/album_sweep.rb
@@ -9,6 +9,10 @@ class AlbumSweep < BaseSweep
                "SELECT type,count(id) FROM albums GROUP BY type ORDER BY type")
     grouped_query(db, "Albums.privacy", span,
                "SELECT privacy,count(id) FROM albums GROUP BY privacy ORDER BY privacy")
+    grouped_query(db, "Albums.GroupAlbum.privacy", span,
+               "SELECT privacy,count(id) FROM albums where type = 'GroupAlbum' GROUP BY privacy ORDER BY privacy")
+    grouped_query(db, "Albums.ProfileAlbum.privacy", span,
+               "SELECT privacy,count(id) FROM albums where type = 'ProfileAlbum' GROUP BY privacy ORDER BY privacy")
   end
 
   # this gets called at high frequency so only collect a few interesting stats
