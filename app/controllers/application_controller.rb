ede629fce070450b2352ba21d067b7b842acf6d0
gregseitz <gseitz@gmail.com>
2011-06-08 08:23:50 -0700

    Added chart to gecko auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def protect_with_http_auth
-    allowed = Set.new(
+    @@allowed ||= Set.new [
 
-    )
+    ]
     # special case for gecko and it's wacky scheme
     # authenticated with special user/pw
-    gecko_custom = Set.new(
-        'gecko#before_after'
-    )
+    @@gecko_custom ||= Set.new [
+        'gecko#before_after',
+        'highcharts#active_users_by_cohort'
+    ]
     basic_auth_user = ZangZingConfig.config[:basic_auth_user]
     basic_auth_password = ZangZingConfig.config[:basic_auth_password]
     method = "#{params[:controller]}##{params[:action]}"
-    unless allowed.include?(method)
+    unless @@allowed.include?(method)
       authenticate_or_request_with_http_basic('ZZRollup') do |username, password|
         ok = false
-        if gecko_custom.include?(method)
+        if @@gecko_custom.include?(method)
           # special case the gecko board requests because they always pass X as password
           gecko_user = ZangZingConfig.config[:gecko_user]
           gecko_password = ZangZingConfig.config[:gecko_password]
