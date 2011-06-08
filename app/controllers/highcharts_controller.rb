b2ea3d898be72b27019fdaf92ca379e84fe44094
duhast <shtepsel_duhast@mail.ru>
2011-06-09 01:30:20 +0300

    refactorings; #2167 chart 2 done

--- app/controllers/highcharts_controller.rb
+++ app/controllers/highcharts_controller.rb
@@ -1,12 +1,10 @@
 class HighchartsController < ApplicationController
+  before_filter :setup_parameters
 
   def active_users_by_cohort
-    span = params[:span] || 1440 #43200 1440
-    span_code = RollupTasks.kind(span)
-
     chart_cfg = {
       :chart => {
-        :renderTo => params[:action].gsub('_', '-'),
+        :renderTo => '',
         :defaultSeriesType => 'area'
       },
       :credits => {
@@ -16,7 +14,7 @@ class HighchartsController < ApplicationController
         :text => 'Cumulative Active Users (10+ Photos) by Cohort'
       },
       :subtitle => {
-        :text => span_code.humanize
+        :text => @span_code.humanize
       },
       :legend => {
         :layout => 'vertical'
@@ -51,12 +49,82 @@ class HighchartsController < ApplicationController
       }
     }
 
-    x_ticks_format = case span_code
+    data = fetch_and_prepare('Cohort.photos_10.%')
+    chart_cfg[:xAxis][:categories] = data[:categories]
+    chart_cfg[:series] = data[:series].reverse
+
+    render :json => chart_cfg
+  end
+
+  def active_users_percent_by_cohort
+    chart_cfg = {
+      :chart => {
+        :renderTo => '',
+        :defaultSeriesType => 'line'
+      },
+      :credits => {
+        :enabled => false
+      },
+      :title => {
+        :text => 'Cumulative % Active Users (10+ Photos) by Cohort'
+      },
+      :subtitle => {
+        :text => @span_code.humanize
+      },
+      :legend => {
+        :layout => 'vertical'
+      },
+      :xAxis => {
+        :tickmarkPlacement => 'on',
+        :title => {
+          :enabled => false
+        },
+        :labels => {
+            :rotation => -90,
+            :align => 'right',
+            :y => 3,
+            :x => 4
+        }
+      },
+      :yAxis => {
+        :title => {
+          :text => '% Active Users (Add 10+ Photos/month)'
+        },
+        :min => 0,
+        :labels => { :formatter => nil }
+      },
+      :tooltip => { :formatter => nil }
+    }
+
+    users_data = fetch_and_prepare('Cohort.users.%')
+    photos10_data = fetch_and_prepare('Cohort.photos_10.%')
+    
+    percent_series = photos10_data[:series].enum_with_index.map do |serie, cohort|
+      percent_serie_data = serie[:data].enum_with_index.map do |val, idx|
+        val = val.to_f / users_data[:series][cohort][:data][idx]
+        val.nan? ? nil : val
+      end
+      {:name => serie[:name], :data => percent_serie_data}
+    end
+
+    chart_cfg[:xAxis][:categories] = photos10_data[:categories]
+    chart_cfg[:series] = percent_series.reverse
+
+    render :json => chart_cfg
+  end
+
+protected
+
+  def setup_parameters
+    @span = params[:@span] || 1440 #43200 1440
+    @span_code = RollupTasks.kind(@span)
+
+    @x_ticks_format = case @span_code
       when 'monthly' then '%b %Y'
       when 'daily' then '%m/%d/%y'
       else '%m/%d/%y %H:%i'
     end
-    period = case span_code
+    @period = case @span_code
       when 'monthly' then (6.months.ago..Time.now)
       when 'daily' then (60.days.ago..Time.now)
       when 'hourly' then (4.days.ago..Time.now)
@@ -64,12 +132,14 @@ class HighchartsController < ApplicationController
       when 'quarter-hourly' then (1.day.ago..Time.now)
       else (30.days.ago..Time.now)
     end
+  end
 
-    data = RollupResult.select("DATE_FORMAT(reported_at, '#{x_ticks_format}') AS report_date, cohort, MAX(sum_value) AS value").group(:report_date).group(:cohort).where(:reported_at => period).where('cohort > 0 AND span = ? AND query_name LIKE "Cohort.photos_10.%"', span).order(:report_date)
+  def fetch_and_prepare(query_name_mask, &block)
+    rollup_data_rows = RollupResult.select("DATE_FORMAT(reported_at, '#{@x_ticks_format}') AS report_date, cohort, MAX(sum_value) AS value").group(:report_date).group(:cohort).where(:reported_at => @period).where("cohort > 0 AND span = ? AND query_name LIKE '#{query_name_mask}'", @span).order(:report_date)
     
-    categories = data.map(&:report_date).uniq
+    categories = rollup_data_rows.map(&:report_date).uniq
     series = {}
-    data.each do |row|
+    rollup_data_rows.each do |row|
       unless series[row.cohort]
         series[row.cohort] = categories.inject({}) do |hsh, cat|
           hsh[cat] = nil
@@ -78,20 +148,18 @@ class HighchartsController < ApplicationController
       end
       series[row.cohort][row.report_date] = row.value
     end
-    chart_cfg[:xAxis][:categories] = categories
-    chart_cfg[:series] = series.map do |cohort, values|
+    chart_series = series.map do |cohort, values|
       cohort_beginning_of_month_date = CohortManager.cohort_base + (cohort - 1).months
-      {
-        :name => cohort_beginning_of_month_date.strftime("Cohort %b '%y"),
-        :data => categories.map{|cat| values[cat].to_i } #This should keep the order
-      }
-    end.reverse
-
-    render :json => {
-      :type => params[:action],
-      :config => chart_cfg
-    }
-
+      unless block.nil?
+        block.call(categories, cohort_beginning_of_month_date, values)
+      else
+        {
+          :name => cohort_beginning_of_month_date.strftime("Cohort %b '%y"),
+          :data => categories.map{|cat| values[cat].to_i } #This should keep the order
+        }
+      end
+    end
+    {:series => chart_series, :categories => categories}
   end
 
 end
