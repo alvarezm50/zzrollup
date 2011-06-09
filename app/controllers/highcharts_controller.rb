3a85b4317bf8f0925df7d8d63691958b5fe581f5
duhast <shtepsel_duhast@mail.ru>
2011-06-10 01:47:54 +0300

    Chart 3 (Daily Cumulative Registered Users by Cohort)

--- app/controllers/highcharts_controller.rb
+++ app/controllers/highcharts_controller.rb
@@ -1,7 +1,9 @@
 class HighchartsController < ApplicationController
+  include ActionView::Helpers::DateHelper
+
   before_filter :setup_parameters
 
-  def active_users_by_cohort
+  def active_users_by_cohort #Charts 1
     chart_cfg = {
       :chart => {
         :renderTo => '',
@@ -14,7 +16,7 @@ class HighchartsController < ApplicationController
         :text => 'Cumulative Active Users (10+ Photos) by Cohort'
       },
       :subtitle => {
-        :text => @span_code.humanize
+        :text => chart_subtitle
       },
       :legend => {
         :layout => 'vertical'
@@ -56,7 +58,7 @@ class HighchartsController < ApplicationController
     render :json => chart_cfg
   end
 
-  def active_users_percent_by_cohort
+  def active_users_percent_by_cohort #Charts 2
     chart_cfg = {
       :chart => {
         :renderTo => '',
@@ -69,7 +71,7 @@ class HighchartsController < ApplicationController
         :text => 'Cumulative % Active Users (10+ Photos) by Cohort'
       },
       :subtitle => {
-        :text => @span_code.humanize
+        :text => chart_subtitle
       },
       :legend => {
         :layout => 'vertical'
@@ -113,6 +115,67 @@ class HighchartsController < ApplicationController
     render :json => chart_cfg
   end
 
+  def daily_cumulative_registered_users_by_cohort #Charts 3
+    chart_cfg = {
+      :chart => {
+        :renderTo => '',
+        :defaultSeriesType => 'line'
+      },
+      :credits => {
+        :enabled => false
+      },
+      :title => {
+        :text => 'Daily Cumulative Registered Users by Cohort'
+      },
+      :subtitle => {
+        :text => chart_subtitle
+      },
+      :legend => {
+        :layout => 'vertical'
+      },
+      :xAxis => {
+        :tickmarkPlacement => 'on',
+        :title => {
+          :enabled => false
+        },
+        :labels => {
+            :rotation => -45,
+            :align => 'right',
+            :y => 7,
+            :x => 5
+        }
+      },
+      :yAxis => {
+        :min => 0,
+        :title => {
+          :text => 'Active Users (Add 10+ Photos/month)'
+        },
+      }
+    }
+
+    categories = (1..31).map{|day| "Day #{day}"}
+    
+    series = []
+    @x_ticks_format = 'Day %e'
+    (1..CohortManager.cohort_current).each do |cohort|
+      cohort_beginning = CohortManager.cohort_beginning_date(cohort)
+      @period = (cohort_beginning..cohort_beginning.end_of_month)
+      cohort_data = fetch_and_prepare("Cohort.users.#{cohort}") do |this_cohort_categories, cohort_beginning_of_month_date, values|
+        {
+          :name => cohort_beginning_of_month_date.strftime("Cohort %b '%y"),
+          :data => categories.map{|cat| values[cat] ? values[cat].to_i : nil }
+        }
+      end
+      series << (cohort_data[:series].first || {:name => cohort_beginning.strftime("Cohort %b '%y"), :data => Array.new(categories.count) })
+    end
+
+    chart_cfg[:xAxis][:categories] = categories
+    chart_cfg[:series] = series
+
+    render :json => chart_cfg
+  end
+
+
 protected
 
   def setup_parameters
@@ -134,9 +197,13 @@ protected
     end
   end
 
+  def chart_subtitle
+    "#{@span_code.humanize}, #{distance_of_time_in_words(@period.first, @period.last)}"
+  end
+
   def fetch_and_prepare(query_name_mask, &block)
     rollup_data_rows = RollupResult.select("DATE_FORMAT(reported_at, '#{@x_ticks_format}') AS report_date, cohort, MAX(sum_value) AS value").group(:report_date).group(:cohort).where(:reported_at => @period).where("cohort > 0 AND span = ? AND query_name LIKE '#{query_name_mask}'", @span).order(:report_date)
-    
+
     categories = rollup_data_rows.map(&:report_date).uniq
     series = {}
     rollup_data_rows.each do |row|
@@ -149,7 +216,7 @@ protected
       series[row.cohort][row.report_date] = row.value
     end
     chart_series = series.map do |cohort, values|
-      cohort_beginning_of_month_date = CohortManager.cohort_base + (cohort - 1).months
+      cohort_beginning_of_month_date = CohortManager.cohort_beginning_date(cohort)
       unless block.nil?
         block.call(categories, cohort_beginning_of_month_date, values)
       else
