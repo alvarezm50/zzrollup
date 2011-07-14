36b3d6d0368556d5da6fdbaaeacc5420bdfc039c
gregseitz <gseitz@gmail.com>
2011-07-13 22:06:58 -0700

    Added support for nearly all zza event types

--- lib/evt_sweep.rb
+++ lib/evt_sweep.rb
@@ -4,26 +4,33 @@ class EvtSweep < BaseSweep
   end
 
   def self.full(span)
-    minimal(span)
     full_query(db, "Agent.first.use.all", span,
                "SELECT count(*) FROM evts WHERE event = 'agent.first.use'")
+    full_query(db, "Page.visit.user.all", span,
+               "SELECT count(*) FROM evts WHERE event = 'page.visit' AND source = 1")
     grouped_query(db, "Page.visit.user", span,
                "SELECT user_type,count(id) FROM evts WHERE event = 'page.visit' AND source = 1 GROUP BY user_type ORDER BY user_type")
+#    grouped_query(db, "", span, "
+#      SELECT event, count(event) FROM evts WHERE
+#      event LIKE 'user.join' OR
+#      event LIKE 'button.createalbum.click%' OR
+#      event LIKE 'album.add_photos_tab.view' OR
+#      event LIKE 'album.name_tab.view' OR
+#      event LIKE 'album.edit_tab.view' OR
+#      event LIKE 'album.privacy_tab.view' OR
+#      event LIKE 'album.contributors_tab.view' OR
+#      event LIKE 'album.share_tab.view' OR
+#      event LIKE 'album.done.click' OR
+#      event LIKE 'email.welcome%' OR
+#      event LIKE 'email.albumshared%' OR
+#      event LIKE 'email.contributorinvite%'
+#      GROUP BY event
+#      ")
+    # grab just about everything
     grouped_query(db, "", span, "
-      SELECT event, count(event) FROM evts WHERE
-      event LIKE 'user.join' OR
-      event LIKE 'button.createalbum.click%' OR
-      event LIKE 'album.add_photos_tab.view' OR
-      event LIKE 'album.name_tab.view' OR
-      event LIKE 'album.edit_tab.view' OR
-      event LIKE 'album.privacy_tab.view' OR
-      event LIKE 'album.contributors_tab.view' OR
-      event LIKE 'album.share_tab.view' OR
-      event LIKE 'album.done.click' OR
-      event LIKE 'email.welcome%' OR
-      event LIKE 'email.albumshared%' OR
-      event LIKE 'email.contributorinvite%'
-      GROUP BY event
+      SELECT event, count(event) FROM evts where
+      event not like 'agent.%.run' and event not like '.%' and event not like ''
+      group by event
       ")
   end
 
