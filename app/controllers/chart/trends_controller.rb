68f0af909125f6314dc0b25af351cdd156483415
duhast <shtepsel_duhast@mail.ru>
2011-07-24 19:49:33 +0300

    #2470 Charts from 'Final Charts' of zza-query-1.xlsx added

--- app/controllers/chart/trends_controller.rb
+++ app/controllers/chart/trends_controller.rb
@@ -35,7 +35,7 @@ class Chart::TrendsController < HighchartsController
           },
           :yAxis => {
             :title => {
-              :text => 'Average # of Photos'
+              :text => 'Number of Photos'
             },
           },
           :plotOptions => {
@@ -53,6 +53,239 @@ class Chart::TrendsController < HighchartsController
       end
     end
   end
+  
+  def photos_per_album_avg
+    photos_src = TrendsDatasource.new(:query_name_mask => 'Photos.all', :calculate_now => true)
+    albums_src = TrendsDatasource.new(:query_name_mask => 'Albums.all', :calculate_now => true)
+    
+    series = photos_src.chart_series.enum_with_index.map do |serie, i|
+      data = serie[:data].enum_with_index.map do |val, idx|
+        res_val = (val.to_f / albums_src.chart_series[i][:data][idx]) rescue nil
+        (res_val.nil? || res_val.nan? || res_val.infinite?) ? nil : (res_val*100).round/100.0
+      end
+      {:name => serie[:name], :data => data}
+    end
+
+    respond_to do |wants|
+      wants.xls do
+        send_xls(photos_src, series)
+      end
+      wants.json do
+        render :json => {
+          :series => series,
+          :chart => {
+            :renderTo => '',
+            :defaultSeriesType => 'line'
+          },
+          :credits => {
+            :enabled => false
+          },
+          :title => {
+            :text => 'Average Photos per Album'
+          },
+          :subtitle => {
+            :text => 'Weekly'
+          },
+          :legend => {
+            :enabled => false
+          },
+          :xAxis => {
+            :categories => photos_src.categories,
+            :tickmarkPlacement => 'on',
+            :title => {
+              :enabled => false
+            },
+            :labels => {
+              :rotation => -90,
+              :align => 'right',
+              :y => 3,
+              :x => 4
+            }
+          },
+          :yAxis => {
+            :title => {
+              :text => 'Average # of Photos'
+            },
+          },
+          :plotOptions => {
+            :area => {
+              :stacking => 'normal',
+              :lineColor => '#666666',
+              :lineWidth => 1,
+              :marker => {
+                :lineWidth => 1,
+                :lineColor => '#666666'
+              }
+            }
+          }
+        }
+      end
+    end    
+  end
+
+  def total_albums
+    albums_src = TrendsDatasource.new(:query_name_mask => 'Albums.all', :calculate_now => true)
+
+    respond_to do |wants|
+      wants.xls do
+        send_xls(albums_src)
+      end
+      wants.json do
+        render :json => {
+          :series => albums_src.chart_series,
+          :chart => {
+            :renderTo => '',
+            :defaultSeriesType => 'line'
+          },
+          :credits => {
+            :enabled => false
+          },
+          :title => {
+            :text => 'Total Albums'
+          },
+          :legend => {
+            :enabled => false
+          },
+          :xAxis => {
+            :categories => albums_src.categories,
+            :tickmarkPlacement => 'on',
+            :title => {
+              :enabled => false
+            },
+            :labels => {
+              :rotation => -90,
+              :align => 'right',
+              :y => 3,
+              :x => 4
+            }
+          },
+          :yAxis => {
+            :title => {
+              :text => 'Number of Albums'
+            },
+          },
+          :plotOptions => {
+            :area => {
+              :stacking => 'normal',
+              :lineColor => '#666666',
+              :lineWidth => 1,
+              :marker => {
+                :lineWidth => 1,
+                :lineColor => '#666666'
+              }
+            }
+          }
+        }
+      end
+    end
+  end
+
+
+  def total_photos
+    albums_src = TrendsDatasource.new(:query_name_mask => 'Photos.all', :calculate_now => true)
+
+    respond_to do |wants|
+      wants.xls do
+        send_xls(albums_src)
+      end
+      wants.json do
+        render :json => {
+          :series => albums_src.chart_series,
+          :chart => {
+            :renderTo => '',
+            :defaultSeriesType => 'line'
+          },
+          :credits => {
+            :enabled => false
+          },
+          :title => {
+            :text => 'Total Photos'
+          },
+          :legend => {
+            :enabled => false
+          },
+          :xAxis => {
+            :categories => albums_src.categories,
+            :tickmarkPlacement => 'on',
+            :title => {
+              :enabled => false
+            },
+            :labels => {
+              :rotation => -90,
+              :align => 'right',
+              :y => 3,
+              :x => 4
+            }
+          },
+          :yAxis => {
+            :title => {
+              :text => 'Number of Photos'
+            },
+          },
+          :plotOptions => {
+            :area => {
+              :stacking => 'normal',
+              :lineColor => '#666666',
+              :lineWidth => 1,
+              :marker => {
+                :lineWidth => 1,
+                :lineColor => '#666666'
+              }
+            }
+          }
+        }
+      end
+    end
+  end
+
+
+  def photos_per_day_monthly
+    data_src = DailyGrowthDatasource.new(:query_name_mask => 'Photos.all', :calculate_now => true)
+
+    categories = []
+    values = []
+    
+    data_src.chart_series.reverse.each do |serie|
+      categories << serie[:name]
+      values << serie[:data].compact.inject(0){|s,e| s+e }
+    end
+    data_src.categories = categories
+    series = [{:name => 'Number', :data => values}]
+
+    respond_to do |wants|
+      wants.xls do
+        send_xls(data_src, series)
+      end
+      wants.json do
+        render :json => {
+          :chart => {
+            :renderTo => '',
+            :defaultSeriesType => 'column'
+          },
+          :series => series,
+          :xAxis => {
+            :categories => categories
+          },
+          :title => {
+            :text => 'Average Photos/Day per Month'
+          },
+          :yAxis => {
+            :labels => { :formatter => nil },
+            :title => {
+              :text => 'Average Photos Per Day'
+            },
+          },
+          :legend => {
+            :enabled => false
+          },
+          :credits => {
+            :enabled => false
+          }
+        }
+      end
+    end
+  end
+
 
 
 end
--- lib/trends_datasource.rb
+++ lib/trends_datasource.rb
@@ -17,19 +17,21 @@ protected
     @period ||= default_period
     @x_labels_format = '%Y-%m-%d'
     @span = RollupTasks::DAILY_REPORT_INTERVAL
-    
 
-    fields_to_select = []
-    conditions = []
-    group_by = []
+    fields_to_select = [
+      "DATE_FORMAT(SUBDATE(`reported_at`, INTERVAL WEEKDAY(`reported_at`) DAY), '#{@x_labels_format}') AS report_date",
+      "ROUND(AVG(sum_value)) AS value",
+      "DATE_FORMAT(reported_at, '%v %x') AS weekyear"
+    ]
+    conditions = [
+      RollupResult.public_sanitize_sql(:reported_at => @period),
+      RollupResult.public_sanitize_sql(["span = ? AND query_name = ?", @span, @query_name_mask]),
+      RollupResult.public_sanitize_sql('sum_value > 0')
+    ]
+    group_by = [
+      'weekyear'
+    ]
 
-    fields_to_select << "DATE_FORMAT(reported_at, '#{@x_labels_format}') AS report_date"
-    fields_to_select << "MAX(sum_value) AS value"
-    group_by << 'report_date'
-    
-    conditions << RollupResult.public_sanitize_sql(:reported_at => @period)
-    conditions << RollupResult.public_sanitize_sql(["span = ? AND query_name = ?", @span, @query_name_mask])
-    
     sql = <<-SQL
       SELECT #{fields_to_select.join(',')} FROM `rollup_results` 
       WHERE (#{conditions.join(') AND (')}) GROUP BY #{group_by.join(',')} ORDER BY `report_date`
@@ -44,7 +46,7 @@ protected
         :data => @rollup_data_rows.inject(Array.new(@categories.size)) do |result_data, row|
           dest_cat = @category_formatter.call(row['report_date'])
           if idx = @categories.index(dest_cat)
-            result_data[idx] = row['value']
+            result_data[idx] = row['value'].to_i
           end
           result_data
         end
