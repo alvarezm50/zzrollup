458677e252f2c94ce80ad4f096480d7d5899d801
duhast <shtepsel_duhast@mail.ru>
2011-08-09 02:23:22 +0300

    #2588 Chart 8

--- app/controllers/chart/photo_sources_controller.rb
+++ app/controllers/chart/photo_sources_controller.rb
@@ -249,6 +249,82 @@ class Chart::PhotoSourcesController < HighchartsController
     end
   end
 
+  def breakdown
+    data_src = PhotosourcesDatasource.new(
+      :calculate_now => true,
+      :percent_view => true,
+      :whole_history => true,
+      :queries_to_fetch => %w(email facebook flickr instagram kodak photobucket picasaweb shutterfly smugmug zangzing fs.osx iphoto.osx picasa.osx fs.win picasa.win simple.osx simple.win)
+    )
+
+    total = data_src.chart_series.inject(0.0) do |sum, s|
+      sum + s[:data].compact.max
+    end
+    series = data_src.chart_series.dup
+    series.each do |s|
+      s[:data] = s[:data].map{|v| (v || 0.0) / total }
+    end
+
+
+    respond_to do |wants|
+      wants.xls do
+        send_xls(data_src)
+      end
+      wants.json do
+        render :json => {
+          :series => series,
+          :chart => {
+            :renderTo => '',
+            :defaultSeriesType => 'line'
+          },
+          :credits => {
+            :enabled => false
+          },
+          :title => {
+            :text => 'Photo Source Breakdown Over Time'
+          },
+          :subtitle => {
+            :text => 'Percentage'
+          },
+          :xAxis => {
+            :categories => data_src.categories,
+            :tickmarkPlacement => 'on',
+            :title => {
+              :enabled => false
+            },
+            :labels => {
+              :rotation => -90,
+              :align => 'right',
+              :y => 3,
+              :x => 4,
+              :step => (data_src.categories.size/30.0).ceil
+            }
+          },
+          :yAxis => {
+            :title => {
+              :text => '% of Photos'
+            },
+            :labels => {:formatter => nil},
+            :min => 0.0
+            #:max => 1.0
+          },
+          :tooltip => {:formatter => nil},
+          :plotOptions => {
+            :area => {
+              :stacking => 'normal',
+              :lineColor => '#666666',
+              :lineWidth => 1,
+              :marker => {
+                :lineWidth => 1,
+                :lineColor => '#666666'
+              }
+            }
+          }
+        }
+      end
+    end
+  end
+
 
 
 end
