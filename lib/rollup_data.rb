f4776a248184070fc69930d8004cb21be0faaed4
duhast <shtepsel_duhast@mail.ru>
2011-09-02 03:47:34 +0300

    fix

--- lib/rollup_data.rb
+++ lib/rollup_data.rb
@@ -2,7 +2,6 @@ require 'rollup_data/humanization'
 require 'rollup_data/colorization'
 require 'rollup_data/universal_datasource'
 require 'rollup_data/cohorts_datasource'
-require 'rollup_data/trends_datasource'
 require 'rollup_data/daily_growth_datasource'
 
 module RollupData
