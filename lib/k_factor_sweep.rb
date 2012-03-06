3e4841063d72c544d1b3c87837e8cebbf0e005da
duhast <shtepsel_duhast@mail.ru>
2012-03-06 20:53:31 +0200

    #4722 KFactorSweeper update

--- lib/k_factor_sweep.rb
+++ lib/k_factor_sweep.rb
@@ -24,11 +24,11 @@ class KFactorSweep < BaseSweep
       raise ArgumentError.new("Unsupported set type - #{set_type}")
     end
     completed_invitations_sent_query = <<-SQL
-      select count(distinct invitations.id) from invitations, tracked_links
+      select count(distinct invitations.id) from invitations, tracked_links, users
         where invitations.tracked_link_id = tracked_links.id
           and tracked_links.created_at > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY)
           and invitations.status = 'complete' and tracked_links.shared_to ='email'
-          and tracked_links.user_id in (select id from users where #{time_field} > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY))
+          and tracked_links.user_id = users.id and users.#{time_field} > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY)
     SQL
     users_count_query = "select count(*) from users where #{time_field} > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY)"
 
@@ -36,7 +36,7 @@ class KFactorSweep < BaseSweep
     users_count = db.select_value(users_count_query)
 
     k = completed_invitations_sent.to_f / users_count
-    k.nan? ? nil : k.round
+    k.nan? ? nil : k*100
   end
 
 end
