6d3221357592e97d2cf3c3dc42ca3496c09b714f
jeremyhermann <jeremyhermann@gmail.com>
2012-03-08 22:27:15 -0800

    fix some k-factor stuff

--- lib/k_factor_sweep.rb
+++ lib/k_factor_sweep.rb
@@ -23,19 +23,35 @@ class KFactorSweep < BaseSweep
     else
       raise ArgumentError.new("Unsupported set type - #{set_type}")
     end
+
+    # this is the number of invitations that were sent and completed
+    # during time period by active or joined users
     completed_invitations_sent_query = <<-SQL
       select count(distinct invitations.id) from invitations, tracked_links, users
         where invitations.tracked_link_id = tracked_links.id
           and tracked_links.created_at > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY)
-          and invitations.status = 'complete' and tracked_links.shared_to ='email'
-          and tracked_links.user_id = users.id and users.#{time_field} > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY)
+          and invitations.status = 'complete'
+          and tracked_links.user_id = users.id
+          and users.#{time_field} > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY)
     SQL
+
+    # this is the total number of active or joined users during time perion
     users_count_query = "select count(*) from users where #{time_field} > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY)"
+
+    # we need to remove these because they joined from an invite during this period and so shouldn't count against us
+    users_joined_from_invite_query = <<-SQL
+        select count(distinct users.id) from invitations, tracked_links, users
+           where invitations.tracked_link_id = tracked_links.id
+             and tracked_links.created_at > DATE_ADD(NOW(), INTERVAL -#{days_count} DAY)
+             and invitations.status = 'complete'
+		         and invitations.invited_user_id = users.id
+    SQL
 
     completed_invitations_sent = db.select_value(completed_invitations_sent_query)
     users_count = db.select_value(users_count_query)
+    users_joined_from_invite_count = db.select_value(users_joined_from_invite_query)
 
-    k = completed_invitations_sent.to_f / users_count
+    k = completed_invitations_sent.to_f / (users_count - users_joined_from_invite_count)
     k.nan? ? nil : k*100
   end
 
